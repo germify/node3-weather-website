27533f9899c038ebb5096574eeac2a613e75392e
Germ <germ@germs-mbp.lan>
2020-01-13 11:12:59 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/afd6a00cb27c0c5d872ab04565e3aae4/' +
+    encodeURIComponent(longitude) +
+    ',' +
+    encodeURIComponent(latitude);
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!');
+    } else if (body.error) {
+      callback('Unable to find location!');
+    } else {
+      callback(
+        undefined,
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
